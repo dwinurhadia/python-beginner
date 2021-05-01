d29f0397d091c4dbdd47aa535a31ca5e6d3076aa
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-01 13:28:55 +0700

    add new variable

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,12 @@ print('Hello world')
 print('o-------')
 print('  !!!!!')
 print('*' * 10)
+
+
+price = 10
+print('price', price)
+price = 2.5
+print('price', price)
+name = 'Kodok'
+is_publish = False
+not_publish = True
