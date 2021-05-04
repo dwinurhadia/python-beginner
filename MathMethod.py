cf273604e27f814adb4bfd740c0ebe6ca9434b2a
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-04 14:56:45 +0700

    math function method python

--- MathMethod.py
+++ MathMethod.py
@@ -1,9 +1,15 @@
 print(10/3)
 # +-*/% // << operator matematika biasa
 
-x = 10  + 3 * 2
+x = (10  + 3) * 2 ** 4
 print(x)
 
 # exponential 2 ** 2 << pangkat
 # multiplication or division
 # addition or substraction
+
+x = 2.9
+print(round(x))
+print(abs(-2.1))
+
+# https://docs.python.org/3/library/math.html
