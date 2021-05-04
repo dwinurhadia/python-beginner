18bc67aa4ccd03dad788fd5cac684ed6db10bc6b
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-04 14:13:54 +0700

    method string

--- StringMethod.py
+++ StringMethod.py
@@ -3,3 +3,18 @@ print(len(course))
 print(course.upper())
 print(course.lower())
 print(course.find('P'))
+print(course.find('b'))
+print(course.find('B'))
+print(course.find('beginner'))
+print(course.replace('beginner','Absolute Beginner'))
+print('Python' in course)
+print('python' in course)
+
+# resume
+# len()
+# course.upper()
+# course.lower()
+# course.title()
+# course.find()
+# course.replace()
+# '...' in course
