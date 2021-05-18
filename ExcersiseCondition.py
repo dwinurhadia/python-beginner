87a0401b34c595633c727b26664f14c4d34516ee
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-18 22:46:44 +0700

    complete exercise python

--- ExcersiseCondition.py
+++ ExcersiseCondition.py
@@ -1,2 +1,9 @@
-weight = input("Weight:")
-print(weight)
+weight = input("Weight : ")
+# print(weight)
+unit = input("(L)bs or (K)g : ")
+if unit == 'L':
+    print("Your weight is", float(weight)/2.2046, "kilos")
+elif unit == 'K':
+    print("Your weight is", float(weight)*2.2046, "lbs")
+else:
+    print("Make sure your unit correct")
