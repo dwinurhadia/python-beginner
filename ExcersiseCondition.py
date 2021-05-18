85124702f2b0e74fb4b36cc0265d1abf7df883fc
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-18 22:50:44 +0700

    add upper() function to make sure user enter correct input

--- ExcersiseCondition.py
+++ ExcersiseCondition.py
@@ -1,9 +1,9 @@
 weight = input("Weight : ")
 # print(weight)
 unit = input("(L)bs or (K)g : ")
-if unit == 'L':
+if unit.upper() == 'L':
     print("Your weight is", float(weight)/2.2046, "kilos")
-elif unit == 'K':
+elif unit.upper() == 'K':
     print("Your weight is", float(weight)*2.2046, "lbs")
 else:
     print("Make sure your unit correct")
