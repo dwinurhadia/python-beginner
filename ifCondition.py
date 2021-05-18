4ab53ed0f22d8679fbbc520399e57bcdd33529ef
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-18 11:12:08 +0700

    add conditions

---
+++ ifCondition.py
@@ -0,0 +1,8 @@
+is_hot = False
+if is_hot:
+    print("It is a hot day")
+    print("Drink plenty a water")
+else:
+    print("It's ia a cold day")
+    print("Wear warm clothes my brother")
+print("Enjoy your day")
