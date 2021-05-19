27977e8979176a1213b62da96f32114b4abeec93
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-20 06:50:43 +0700

    list find max number

--- ListsName.py
+++ ListsName.py
@@ -1,9 +1,16 @@
-name = ['ayam','ikan','tempe','sop']
-print(name)
-print(name[2])
-print(name[0])
-print(name[2:])
-print(name[-2])
-print(name[2:3])
-name[0] = 'kodok'
-print(name)
+# name = ['ayam','ikan','tempe','sop']
+# print(name)
+# print(name[2])
+# print(name[0])
+# print(name[2:])
+# print(name[-2])
+# print(name[2:3])
+# name[0] = 'kodok'
+# print(name)
+
+numbers = [3, 6, 9, 2, 6]
+max = numbers[0]
+for numb in numbers:
+    if numb > max:
+        max = numb
+print(max)
