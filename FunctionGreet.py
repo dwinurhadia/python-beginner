a1fdd6e86e5ba64e1dd9342718ed9a13440e8f8a
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-23 18:16:59 +0700

    Function add new parameter

--- FunctionGreet.py
+++ FunctionGreet.py
@@ -5,3 +5,9 @@ def getting_message():
 print("Starting")
 getting_message()
 print("Finish")
+
+def greeting_name(first_name, last_name):
+    # print("Hello "+ first_name +" welcome to indonesiam")
+    print(f"Hi {first_name} {last_name} welcome to indonesia")
+
+greeting_name('Paul','Walker')
