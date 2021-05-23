acf783dad0d06fbcd94594d090122a8e9fe34ae9
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-23 18:34:02 +0700

    Function return value

--- FunctionGreet.py
+++ FunctionGreet.py
@@ -12,3 +12,9 @@ def greeting_name(first_name, last_name):
 
 greeting_name('Paul','Walker')
 greeting_name('Paul', last_name='Walker')
+
+def calculate(number):
+    # return number * number
+    print(number*number)
+    # jika function tidak bisa return maka akan menampilkan nilai None ( atau Null di Java/php/c#)
+print(calculate(4))
