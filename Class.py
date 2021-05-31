a50e2ef8c11339335d5ba93e6bcfd0763a4f11c9
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2021-05-31 15:35:44 +0700

    add new class to inheritance or init value

--- Class.py
+++ Class.py
@@ -1,17 +1,11 @@
 class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
     def move(self):
         print("move")
     def draw(self):
         print("self")
 
-point1 = Point()
-point1.draw()
-point1.move()
-point1.x = 10
-point1.y = 20
-print(point1.x)
-point1.draw()
-
-point2 = Point()
-point2.x = 40
-print(point2.x)
+point = Point(20,30)
+print(point.x)
